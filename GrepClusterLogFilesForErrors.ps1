4740dfe66dcead1aff7d9604e3e7ba64b37a78f1
John Morehouse <john@jmorehouse.com>
2018-12-11 10:20:12 -0500

    initial commit
    script will basically grep files for ERR lines

---
+++ GrepClusterLogFilesForErrors.ps1
@@ -0,0 +1,11 @@
+﻿
+
+
+$dir = "C:\users\john\Desktop\Temp\"
+
+$files = Get-ChildItem $dir -Recurse -Include "*.log"
+
+foreach ($file in $files){
+    $out = "ERR_" + $file.BaseName + ".txt"
+    select-string -path $file.FullName -Pattern " ERR " -AllMatches | out-file "$dir\$out"
+}
